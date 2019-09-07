c2b40551e5c9cf0ace7eef91c6195848bca70dc6
杨丰畅 <ayang818@qq.com>
2019-09-07 16:15:34 +0800

    (Debug)修复一些NPE问题

---
+++ src/main/java/top/ayang818/pfstudio/mapper/CommentExtMapper.java
@@ -0,0 +1,11 @@
+package top.ayang818.pfstudio.mapper;
+
+/**
+ * @ClassName CommentExtMapper
+ * @Dessription TODO
+ * @Author 杨丰畅
+ * @Date 2019/9/7 14:14
+ **/
+public interface CommentExtMapper {
+    int countPages();
+}
